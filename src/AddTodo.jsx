45e42f83744f45484349e0a2d5e829bf6589665c
PlutoA713N <mallachandu99@gmail.com>
2024-04-15 18:31:28 +0530

    Add AddTodo.jsx

---
+++ src/AddTodo.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+const AddTodo = ({ onAdd }) => {
+    const [text, setText] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log( text, 44)
+        onAdd(text)
+        setText('')
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input 
+                type='text'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Enter a new todo..."
+            />
+            <button type="submit">Add Todo</button>
+        </form>
+    )
+
+}
+
+export default AddTodo
